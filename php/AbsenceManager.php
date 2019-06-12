ed825a9a32a3fae6c1f3f10271a8219365008564
Theault Harruis <21609656@campus.unicaen.fr>
2019-06-12 16:33:38 +0200

    ajout AbsenceManager

--- php/AbsenceManager.php
+++ php/AbsenceManager.php
@@ -4,7 +4,12 @@ require_once("Absence.php");
 
 class AbsenceManager{
 
-    private static $GET_ALL_ABSENCE = 'SELECT mod.CODEMODULE, tea.LASTNAMETEACHER, N_STUDENT, les.CODETYPE, hos.LASTNAMEHEADOFSTUDY, DATEBEGIN, DATEEND FROM ABSENCE abs,  ';
+    private static $GET_ALL_ABSENCE = 'SELECT mod.CODEMODULE, tea.LASTNAMETEACHER, stu.N_STUDENT, les.CODETYPE, hos.LASTNAMEHEADOFSTUDY, DATEBEGIN, DATEEND FROM ABSENCE abs
+										join MODULE mod using(N_TYPE);
+										join TEACHER tea using(N_TEACHER)
+										join LESSON_TYPE using(N_TYPE)
+										join HEAD_OF_STUDY using(N_HEADOFSTUDY)
+										join STUDENT using(N_STUDENT)';
 
     static function selectCurrent(){
 
