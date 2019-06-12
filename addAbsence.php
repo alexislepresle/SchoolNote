69c87bc58ca5c2b9b0e26f324f6926637496f66e
Luis Thein <luis.thein@gmx.de>
2019-06-12 16:38:19 +0200

    Add NavBAr to addAbsence

--- addAbsence.php
+++ addAbsence.php
@@ -15,70 +15,107 @@
   <title>Document</title>
 </head>
 
-<body class="form-input">
-  <div class="card">
-    <header class="card-header">
-      <p class="card-header-title">
-        Add Student absent
-      </p>
-    </header>
-    <div class="card-content">
-      <div class="field is-grouped">
-        <p class="control has-icons-left has-icons-right">
-          <input type="text" autocomplete="off" id="datepicker" class="input" placeholder="Date">
-          <span class="icon is-small is-left">
-            <i class="far fa-calendar-alt"></i>
-          </span>
-        </p>
-        <p class="control has-icons-left has-icons-right">
-          <input type="text" autocomplete="off" id="timepicker" class="input" placeholder="Date">
-          <span class="icon is-small is-left">
-            <i class="far fa-clock"></i>
-          </span>
-        </p>
+<body>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <h1 class="title"> SchoolNote</h1>
+      </a>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          Add an absence
+        </a>
       </div>
-      <div class="field is-grouped">
-        <div class="control">
-          <div class="select">
-            <select>
-              <option>Modules</option>
-              <option>Temporary options</option>
-            </select>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-dark">
+              <i class="fas fa-bell"></i>
+            </a>
+            <a class="button is-light">
+              Log out
+            </a>
           </div>
-        </div>
-        <div class="control">
-          <div class="select">
-            <select>
-              <option>UE</option>
-              <option>1.1</option>
-              <option>1.2</option>
-              <option>2.1</option>
-              <option>2.2</option>
-            </select>
-          </div>
-        </div>
-        <div class="control">
-          <div class="select">
-            <select>
-              <option>Type</option>
-              <option>EM</option>
-              <option>TD</option>
-              <option>CC</option>
-              <option>TP</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      <div class="field">
-        <div class="control">
-          <input class="input" type="text" placeholder="Name of the Student">
         </div>
       </div>
     </div>
-    <footer class="card-footer">
-      <a href="#" class="card-footer-item">Send</a>
-      <a href="#" class="card-footer-item">Cancel</a>
-    </footer>
+  </nav>
+  <div class="form-input">
+
+
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          Add Student absent
+        </p>
+      </header>
+      <div class="card-content">
+        <div class="field is-grouped">
+          <p class="control has-icons-left has-icons-right">
+            <input type="text" autocomplete="off" id="datepicker" class="input" placeholder="Date">
+            <span class="icon is-small is-left">
+              <i class="far fa-calendar-alt"></i>
+            </span>
+          </p>
+          <p class="control has-icons-left has-icons-right">
+            <input type="text" autocomplete="off" id="timepicker" class="input" placeholder="Date">
+            <span class="icon is-small is-left">
+              <i class="far fa-clock"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field is-grouped">
+          <div class="control">
+            <div class="select">
+              <select>
+                <option>Modules</option>
+                <option>Temporary options</option>
+              </select>
+            </div>
+          </div>
+          <div class="control">
+            <div class="select">
+              <select>
+                <option>UE</option>
+                <option>1.1</option>
+                <option>1.2</option>
+                <option>2.1</option>
+                <option>2.2</option>
+              </select>
+            </div>
+          </div>
+          <div class="control">
+            <div class="select">
+              <select>
+                <option>Type</option>
+                <option>EM</option>
+                <option>TD</option>
+                <option>CC</option>
+                <option>TP</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <input class="input" type="text" placeholder="Name of the Student">
+          </div>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Send</a>
+        <a href="#" class="card-footer-item">Cancel</a>
+      </footer>
+    </div>
   </div>
 </body>
 
