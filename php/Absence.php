c0556b4b148b341c8c73a388a82107d8e5d8f0ee
Theault Harruis <21609656@campus.unicaen.fr>
2019-06-13 09:31:21 +0200

    modif AbsMan

--- php/Absence.php
+++ php/Absence.php
@@ -2,15 +2,15 @@
 
 class Absence{
 
-    function __construct($codeModule, $lastNameTeacher, $nStudent, $codeType, $lastNameHeadOfStudy, $dateBegin, $dateEnd){
+    function __construct($codeModule, $lastNameTeacher, $nStudent, $codeType, $dateBegin, $dateEnd, $comment){
         $this->codeModule = $codeModule;
         $this->lastNameTeacher = $lastNameTeacher;
         $this->nStudent = $nStudent;
         $this->codeType = $codeType;
-		$this->lastNameHeadOfStudy = $lastNameHeadOfStudy;
 		$this->StatusAbsence = false;
 		$this->dateBegin = $dateBegin;
 		$this->dateEnd = $dateEnd;
+		$this->comment = $comment;
     }
 	
 }
